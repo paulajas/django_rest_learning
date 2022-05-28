b8cd20f78e2a832dc121308f0a256230b346a5ab
paulajas <paulajas@gmail.com>
2022-05-28 21:57:02 +0200

    Fix runscript for MP2

--- masy/receipe/scripts/masy_2.py
+++ masy/receipe/scripts/masy_2.py
@@ -1,9 +1,12 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "masy.settings")
+
 from datetime import datetime
-from masy.receipe.models import CookBook, CookBookReceipe, Country, Picture, Tag
+from receipe.models import CookBook, CookBookReceipe, Country, Picture, Tag
 from receipe.models import MeatReceipe
 
 def run(*args):
-
+    
     # MeatReceipe.objects.create(meat_kind="leg", meat_type="red", name="roasted meat", make_time=30, hard_level="easy", kcal=150, alergens=['protein', 'lactose'], for_children=True, make_date=datetime.now().date()).save()
     Country.objects.create(name="Szwecja").save()
     Tag.objects.create(name="test-tag").save()
@@ -27,7 +30,7 @@ def run(*args):
     print(f"After adding tag: {MeatReceipe.objects.get(pk=1).tag.all()[0].name}")
     CookBookReceipe.objects.create(receipe=MeatReceipe.objects.get(pk=1), cookbook=CookBook.objects.get(pk=1), ratio=1) # asocjacja z atrybutem
     print(f"CookBookReceipe as association with attribute: {CookBookReceipe.objects.get(receipe=MeatReceipe.objects.get(pk=1), cookbook=CookBook.objects.get(pk=1), ratio=1)}")
-    MeatReceipe.objects.get(pk=1).update(picture= Picture.objects.get(pk=1)) # asocjacja kwalifikowana
+    MeatReceipe.objects.filter(pk=1).update(picture= Picture.objects.get(pk=1)) # asocjacja kwalifikowana
     print(f"Picture added as qualified association: {MeatReceipe.objects.get(pk=1).picture} ")
     print(f"Picture also can show receipe: {Picture.objects.get(pk=1).receipe_set.all()}")
     country_test = Country(name="Test_new")
